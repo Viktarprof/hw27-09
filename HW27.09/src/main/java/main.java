fc02d2e3b295b37e3245c46c2b195cbb5cbeb9d3
Viktarprof <113298552+Viktarprof@users.noreply.github.com>
2022-09-29 12:54:22 +0200

    Add files via upload

---
+++ HW27.09/src/main/java/main.java
@@ -0,0 +1,39 @@
+
+public class main {
+
+    public static void main(String[] args) {
+        // 0. Перевести число 478 из шестнадцатеричной в десятичную
+
+        int test = 0x478;
+        System.out.println(test);
+        double result = 4 * Math.pow(16, 2) + 7 * Math.pow(16, 1) + 8 * Math.pow(16, 0); // десятичная 1144
+
+        // 1.Разложить число 200345 на разряды в десятичной системе. не знаю
+        int w = 200345;
+        System.out.println(w);
+        //double result1 = 2 * 10 ^ 5 ;
+
+
+        //2.Перевести 637 из десятичной в шестнадцатеричную и обратно (10->16 и 16->10)
+        System.out.println(" 16 :" +Integer.toHexString(637));
+        //System.out.println(" 10 :" +Integer.??????(637)); не знаю 16->10)
+
+        //3.Перевести 637 из десятичной в двоичную
+        System.out.println(" 2 :" + Integer.toBinaryString(637));
+
+
+        // 4.11100111 перевести в десятичную
+      //  System.out.println("10:"+Integer.????????(11100111)); не знаю 2->10)
+
+        //5.Перевести 637 из десятичной в троичную
+        //System.out.println("3:" + Integer.?????????(637));
+
+        //  !!!!!Прошу объяснить как!!!!
+
+
+    }
+
+
+    }
+
+
